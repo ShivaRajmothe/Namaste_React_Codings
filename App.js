d0506a6dab43be4ea0ec7731ca336a07436aa787
Shivaraj0920 <shivaraj.mothe@cognizant.com>
2026-02-28 18:35:17 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,12 @@
+
+
+
+const parent = React.createElement("div", {id : "parent"}, [React.createElement("div",{id: "child1"},[React.createElement("h1",{id : "heading"}, "Hello I'm H1 "),[React.createElement("h1",{id : "heading"}, "Hello I'm H1 H1 ")]]),
+    React.createElement("div",{id: "child2"},[React.createElement("h1",{id : "heading"}, "Hello I'm H1 ")])
+]);
+
+
+const heading = React.createElement("h1", {id : "heading"}, "Namaste React");
+console.log(heading);
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent);
