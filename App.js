c901978257a150f27c5433f184dc8695493ba5a8
ShivaRajmothe <65208192+ShivaRajmothe@users.noreply.github.com>
2026-03-01 16:58:35 +0000

    episode-02-Parcel-Configuration

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", {id : "parent"}, [React.createElement("div",{id: "child1"},[React.createElement("h1",{id : "heading"}, "Hello I'm H1 "),[React.createElement("h1",{id : "heading"}, "Hello I'm H1 H1 ")]]),
-    React.createElement("div",{id: "child2"},[React.createElement("h1",{id : "heading"}, "Hello I'm H1 ")])
+    React.createElement("div",{id: "child2"},[React.createElement("h1",{id : "heading"}, "Hello I'm Shivaraj ")])
 ]);
 
-const heading = React.createElement("h1", {id : "heading"}, "Namaste React");
+const heading = React.createElement("h1", {id : "heading"}, "Namaste React Shiva");
 console.log(heading);
     const root = ReactDOM.createRoot(document.getElementById("root"));
     root.render(parent); 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 Namaste React
+
+#Parcel
+- Dev Build
+- Production Ready Code
+- Tree Shaking
+- Compressed 
+- Minification
+- Error Handling
+- Bundling
+- differential Bundling
+- https Support 
+- Image Optimization
+- HMR(Hot Module Replacement)
+- File Watching Alogrithm( written in C++)
+- Parcel-Cache
+- Caching - (Faster Build)
+- Code Splitting
+- Consistent Hashing 
+
