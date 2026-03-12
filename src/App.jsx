3061f42be6099949a4e3b59710501847369656c5
Shivaraj0920 <shivaraj.mothe@cognizant.com>
2026-03-12 22:16:13 +0530

    ResturantMenu for Cards

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const appRouter = createBrowserRouter([
         element: <Contact />,
       },
       {
-        path: "/restaurants/:userId",
+        path: "/restaurants/:Id",
         element: <RestaurantMenu />,
       },
     ],
--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,40 +1,39 @@
-import React, { use } from "react";
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import Shimmer from "./Shimmer";
 
+const ResturantMenu = () => {
+  const { id } = useParams(); // route param (not used here)
+  const [resInfo, setResInfo] = useState(null);
 
-const ResturantMenu =() =>
-{
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const[resInfo, setResInfo] = useState(null);
-
-useEffect(() =>{
-    fetchRestaurantMenu();
-}, [])
-
-
-const fetchRestaurantMenu = async () =>
-{
-    const data = await fetch("https://dummyjson.com/recipes");
+  const fetchData = async () => {
+    const data = await fetch("https://dummyjson.com/recipes?id=" + id);
     const json = await data.json();
-    setResInfo(json.recipes);
-    console.log(json.recipes);
-};
+    setResInfo(json);
+    console.log(json);
+  };
 
-const[ name, cuisine, rating, cookTimeMinutes, image] = resInfo.recipes;
-return resInfo === null ? <Shimmer /> : (
+  // Show shimmer until data is loaded
+  if (resInfo === null) return <Shimmer />;
+
+  const { name, cuisine, rating, cookTimeMinutes, ingredients } =
+    resInfo.recipes?.[0] || {};
+
+  console.log({ name });
+
+  return (resInfo === null) ? (<Shimmer />) : (
     <div>
-    <h1>{resInfo?.name}</h1>
-    <h2>Menu</h2>
-    <ul>
-        <li>Biryani</li>
-        <li>Pizza</li>
-        <li>Fried Rice</li>
-        <li>Spring Rolls</li>
-    </ul>
-        </div>
-    )
-}
-
+      <h1>{name}</h1>
+      <h3>{cuisine}</h3>
+      <h4>{ingredients?.join(", ")}</h4>
+      <p>⭐ Rating: {rating}</p>
+      <p>⏱️ Cook Time: {cookTimeMinutes} minutes</p>
+    </div>
+  );
+};
 
 export default ResturantMenu;
